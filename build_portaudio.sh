9c23d9a8855b097294659005a3184868ccb4f2d3
Noarkhh <jakub.pryc@gmail.com>
2023-09-14 14:16:54 +0200

    Correct ghr

--- build_portaudio.sh
+++ build_portaudio.sh
@@ -6,7 +6,7 @@ cd ~/.local
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 alias brew=~/.local/homebrew/bin/brew
 export XML_CATALOG_FILES=`pwd`/etc/xml/catalog
-brew install portaudio  # for linux: brew install --build-from-source portaudio
+brew install --build-from-source portaudio  # for linux: brew install --build-from-source portaudio
 cp -r ~/.local/homebrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
 cp -r ~/.local/homebrew/lib/*.dylib ~/project/workspace/$ARTIFACT_NAME/lib
 
