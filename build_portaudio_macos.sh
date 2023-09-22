07692b447ec750cc936492733a0beac0d8975a20
Noarkhh <jakub.pryc@gmail.com>
2023-09-22 17:48:34 +0200

    Change homebrew installation

---
+++ build_portaudio_macos.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/project/workspace/$ARTIFACT_NAME/include
+mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install portaudio  # for linux: brew install --build-from-source portaudio
+cp -r /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
+cp -r /home/linuxbrew/.linuxbrew/lib/*.dylib.* ~/project/workspace/$ARTIFACT_NAME/lib
+cd ~/project/workspace/$ARTIFACT_NAME/lib
+for f in *.dylib;
+do
+    install_name_tool -id "@rpath/$f" $f
+done
