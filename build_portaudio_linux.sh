0d706c2d6af4e7d201320fdef3e63a8b36da2cdb
Noarkhh <jakub.pryc@gmail.com>
2023-09-25 10:41:15 +0200

    Fix linux script

--- build_portaudio_linux.sh
+++ build_portaudio_linux.sh
@@ -3,6 +3,5 @@ mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 brew install portaudio
-cp -r /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
-cp -r /home/linuxbrew/.linuxbrew/lib/*.so.* ~/project/workspace/$ARTIFACT_NAME/lib
-cd ~/project/workspace/$ARTIFACT_NAME/lib
+cp -Lr /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
+cp -Lr /home/linuxbrew/.linuxbrew/lib/*.so.* ~/project/workspace/$ARTIFACT_NAME/lib
