f4a59c0ca169c9a2d6b2fd0f703d7dd08d881def
Noarkhh <jakub.pryc@gmail.com>
2023-09-25 10:01:37 +0200

    fix scripts

--- build_portaudio_linux.sh
+++ build_portaudio_linux.sh
@@ -2,7 +2,7 @@ mkdir -p ~/project/workspace/$ARTIFACT_NAME/include
 mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-brew install portaudio  # for linux: brew install --build-from-source portaudio
+brew install portaudio
 cp -r /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
 cp -r /home/linuxbrew/.linuxbrew/lib/*.so.* ~/project/workspace/$ARTIFACT_NAME/lib
 cd ~/project/workspace/$ARTIFACT_NAME/lib
--- build_portaudio_macos_intel.sh
+++ build_portaudio_macos_intel.sh
@@ -1,10 +1,9 @@
 mkdir -p ~/project/workspace/$ARTIFACT_NAME/include
 mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-brew install portaudio  # for linux: brew install --build-from-source portaudio
-cp -r /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
-cp -r /home/linuxbrew/.linuxbrew/lib/*.dylib ~/project/workspace/$ARTIFACT_NAME/lib
+brew install portaudio
+cp -r /usr/local/include/* ~/project/workspace/$ARTIFACT_NAME/include
+cp -r /usr/local/lib/*.dylib ~/project/workspace/$ARTIFACT_NAME/lib
 cd ~/project/workspace/$ARTIFACT_NAME/lib
 for f in *.dylib;
 do
