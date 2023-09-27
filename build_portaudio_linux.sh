36e2232015fd1783376a2a6af1b11361fcaf9bc0
Noarkhh <jakub.pryc@gmail.com>
2023-09-27 11:55:20 +0200

    Update orb

--- build_portaudio_linux.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p ~/project/workspace/$ARTIFACT_NAME/include
-mkdir -p ~/project/workspace/$ARTIFACT_NAME/lib
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-brew install portaudio
-cp -Lr /home/linuxbrew/.linuxbrew/include/* ~/project/workspace/$ARTIFACT_NAME/include
-cp -Lr /home/linuxbrew/.linuxbrew/lib/*.so.* ~/project/workspace/$ARTIFACT_NAME/lib
